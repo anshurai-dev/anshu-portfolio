99854f5d8416b01ce3e4aa93383d75add3b17448
Anshu Rai <anshu.rai018@gmail.com>
2024-10-19 21:42:54 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px"
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+
+    fontFamily: {
+      primary: "var(--font-bebas)",
+      secondary: "var(--font-inter)",
+    },
+     
     extend: {
       colors: {
        colors: {
@@ -16,6 +33,10 @@ module.exports = {
           hover: "#79d3cc",
         },
        },
+       boxShadow: {
+        custom: "0px 14px 54px rgba(0,0,0,0.1)",
+       }
+
       },
     },
   },
